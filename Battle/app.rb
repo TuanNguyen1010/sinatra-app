2b4ec90311db266408f8db8c85e4a496351ebc38
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-11-27 18:40:47 +0000

    added attack function

--- Battle/app.rb
+++ Battle/app.rb
@@ -16,10 +16,17 @@ enable :sessions
   end
 
   get '/play' do
+    @hp_1 = 60
+    @hp_2 = 60
     @player_1 = session[:player_1]
     @player_2 = session[:player_2]
     erb(:play)
+  end
 
+  post '/attack' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:attack)
   end
 
 run! if app_file == $0
--- Battle/spec/spec_helper.rb
+++ Battle/spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'features/web_helpers'
 ENV["RACK_ENV"] = "test"
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
